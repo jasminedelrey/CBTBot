b04d5662bd38fb6726bd172e2af7a20e2dfcb3a2
Jasmine Reyes <jreyes5@oxy.edu>
2020-03-22 22:39:56 -0700

    added stuff

--- oxycsbot.py
+++ oxycsbot.py
@@ -64,6 +64,10 @@ class CBTBot(ChatBot):
         'isolated': 'isolated',
         'isolation': 'isolated',
         'worthless': 'worthless',
+        'far from home': 'homesick',
+        'different': 'isolated',
+        "parents don't know": 'unprepared',
+        "parents didn't go to college": 'unprepared',
         'worried': 'worried',
         'worry': 'worried',
         'sad': 'sad',
@@ -115,7 +119,6 @@ class CBTBot(ChatBot):
         'room mate': 'roommate',
         'roommate': 'roommate',
         'living': 'roommate',
-        'lost': 'lost',
         'academic': 'academic',
         'hard': 'hard',
         'alone': 'alone',
@@ -137,17 +140,12 @@ class CBTBot(ChatBot):
 
         'religious': 'religious',
         'cultural': 'cultural',
-        'academic': 'academic',
         'personal': 'personal',
         'residential': 'residential',
 
-        'religious': 'religious',
         'race': 'race',
-        'different': 'different',
-        'academic': 'academic',
         'social': 'social',
         'friends': 'friends',
-        'cultural': 'cultural',
 
 
     }
@@ -232,10 +230,10 @@ class CBTBot(ChatBot):
         'should statement',
     }
 
-    ASSIGNMENTS = {'going to a place of worship.', 'volunteering for a cause you believe in.', 'going to the park.',
-                   'going for a walk.', 'going out for dinner.', 'calling friends and/or family.',
-                   'going out with friends and/or family.', 'working out.', 'getting immersed in art.', 'dancing.',
-                   'singing.', 'watching your favorite movie.', 'journaling your thoughts.', 'slowly breathing in and out.'}
+    ASSIGNMENTS = {'going to a place of worship! ', 'volunteering for a cause you believe in. ', 'going to the park! ',
+                   'going for a walk! ', 'going out for dinner ! ', 'calling friends and/or family. ',
+                   'going out with friends and/or family. ', 'working out! ', 'getting immersed in art! ', 'dancing! ',
+                   'singing ! ', 'watching your favorite movie ! ', 'journaling your thoughts.', 'slowly breathing in and out. '}
 
     def __init__(self):
         """Initialize the OxyCSBot.
@@ -266,8 +264,6 @@ class CBTBot(ChatBot):
             'not enough': 'emotional reasoning',
             'not prepared': 'emotional reasoning',
             'imposter': 'filtering',
-            'unlovable': 'personalization',
-            'abnormal': 'personalization',
             'not confident': 'emotional reasoning',
             'should': 'should statement',
             'succeed': 'catastrophizing',
